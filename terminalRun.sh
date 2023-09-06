fcfac11c2e2eaf641cd0220c9791f9bffb6d1c93
Isabella Chiaravallotti <95446790+isabellajchiara@users.noreply.github.com>
2023-09-06 11:08:34 -0400

    Create terminalRun.sh

---
+++ terminalRun.sh
@@ -0,0 +1,17 @@
+ssh -Y ich@beluga.computecanada.ca
+cd yourDirectory
+
+module load python gcc
+virtualenv --no-download tsinfer
+source tsinfer/bin/activate
+
+pip install tsinfer
+pip install pandas 
+
+salloc --time=3:00:00 --mem=50G --account=def-haricots
+
+python
+
+>>> exec(open('ConvertData.py').read())
+>>> createSampleData(geno, locations) # this function is defined in ConverData.py
+>>> inferred_ts = ts.infer(sample_data) # us ts.infer to infer the tree sequence
